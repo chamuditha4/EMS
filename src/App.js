0c03e5d047e2d0259472c40b4e03e57e4305bb50
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-08-06 22:48:07 +0530

    Navigation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            Hello World!!!
+                <nav>
+                  <ul>
+                    <li><a href="home.html">Home</a></li>
+                    <li><a href="#">Job Portal</a>
+                      <ul>
+                        <li><a href="Login.html">Explore Jobs</a></li>
+                        <li><a href="Login.html">Add Jobs</a></li>
+
+                      </ul>
+                    </li>
+                    <li><a href="Map.html">Find Us</a></li>
+                    <li><a href="about us.html" class="active">About Us</a>
+                      <ul>
+                        <li><a href="about%20us.html#aboutsec">Our Story</a></li>
+                        <li><a href="about%20us.html#missionpa">Our Mission</a></li>
+                        <li><a href="about%20us.html#wherewebac">Where We Work</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="contactUs.html">Contact Us</a></li>
+                  </ul>
+                </nav>
          </div>
       );
    }
