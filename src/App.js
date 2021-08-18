f0db1b08c695559628002572ea96cc63e61e111f
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-08-18 17:49:10 +0530

    Small changes :)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from 'react';
+import styles from './styles/App.css';
+import Header from './Header';
 
 class App extends React.Component {
+  render() {
+     return (
+        <div>
+           <NavigationBar/>
+        </div>
+     );
+  }
+}
+
+class NavigationBar extends React.Component {
    render() {
       return (
          <div>
-                <nav>
-                  <ul>
-                    <li><a href="home.html">Home</a></li>
-                    <li><a href="#">Job Portal</a>
-                      <ul>
-                        <li><a href="Login.html">Explore Jobs</a></li>
-                        <li><a href="Login.html">Add Jobs</a></li>
-
-                      </ul>
-                    </li>
-                    <li><a href="Map.html">Find Us</a></li>
-                    <li><a href="about us.html" class="active">About Us</a>
-                      <ul>
-                        <li><a href="about%20us.html#aboutsec">Our Story</a></li>
-                        <li><a href="about%20us.html#missionpa">Our Mission</a></li>
-                        <li><a href="about%20us.html#wherewebac">Where We Work</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="contactUs.html">Contact Us</a></li>
-                  </ul>
-                </nav>
+           <Header />
+            <div className={styles.home}>
+              <a href="login.html"><button class="butt" id="butt1">Log In</button></a>
+              <a href="Register.html"><button class="butt" id="butt2">Register</button></a>
+            </div>
          </div>
       );
    }
---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <nav>
+            <ul>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="yy.html">Job Portal</a>
+                <ul>
+                <li><a href="Login.html">Explore Jobs</a></li>
+                <li><a href="Login.html">Add Jobs</a></li>
+
+                </ul>
+            </li>
+            <li><a href="Map.html">Find Us</a></li>
+            <li><a href="about us.html" class="active">About Us</a>
+                <ul>
+                <li><a href="about%20us.html#aboutsec">Our Story</a></li>
+                <li><a href="about%20us.html#missionpa">Our Mission</a></li>
+                <li><a href="about%20us.html#wherewebac">Where We Work</a></li>
+                </ul>
+            </li>
+            <li><a href="contactUs.html">Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
