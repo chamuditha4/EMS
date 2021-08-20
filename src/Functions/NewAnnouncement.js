9d42061e488a4571f86ab1ee6d85fad00c0210e7
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-08-21 00:21:01 +0530

    Add Announcement

---
+++ src/Functions/NewAnnouncement.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import  './../styles/App.css';
+
+function NewAnnouncement() {
+    return (
+      <div>
+        <div className="prof">
+          <h2>New Announcement</h2>
+          <TextField id="standard-uncontrolled" label="Title" defaultValue="" /><br></br><br></br>
+          <TextField id="standard-uncontrolled" label="Department" defaultValue="" /><br></br><br></br>
+          <TextField id="outlined-multiline-flexible" label="Announcement" multiline Rows={4} variant="outlined" style = {{width: 350}} /><br></br><br></br>
+          <Button variant="contained" color="primary">
+            Add Announcement
+          </Button><br></br><br></br>
+        </div>
+      </div>
+    )
+  }
+  
+  export default NewAnnouncement
--- src/Tabs/AnnouncementTabs.js
+++ src/Tabs/AnnouncementTabs.js
@@ -5,6 +5,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import NewAnnouncement from '.././Functions/NewAnnouncement';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -74,7 +75,7 @@ export default function AnnouncementTabs() {
         <Tab label="Remove Announcement" {...a11yProps(2)} />
       </Tabs>
       <TabPanel value={value} index={0}>
-        Fuck One
+        <NewAnnouncement/>
       </TabPanel>
       <TabPanel value={value} index={1}>
       Fuck Two
