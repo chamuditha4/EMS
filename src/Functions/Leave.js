4945a3ea818a3d40ef918176a3c81edf7c71f39c
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-08-20 22:54:02 +0530

    % --> px

--- src/Functions/Leave.js
+++ src/Functions/Leave.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 import  './../styles/App.css';
 
 function Leave() {
