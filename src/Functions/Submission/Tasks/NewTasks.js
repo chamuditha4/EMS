1a3b01dce816a93e2ccdcaaab83558899c430ca1
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-05 20:04:44 +0530

    User MAp :)

--- src/Functions/Submission/Tasks/NewTasks.js
+++ src/Functions/Submission/Tasks/NewTasks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState  } from 'react';
 import Button from '@material-ui/core/Button';
 import  './../../../styles/App.css';
 import TextField from '@material-ui/core/TextField';
@@ -6,14 +6,33 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import axios from 'axios';
 
 
+
 function NewTasks() {
-  axios.get('http://localhost:4000/users/employees')
-          .then(res =>{
-            console.log(res)
-          });
+  const [repo,setRepo] = useState([]);
+
+  const getRepo = () => {
+    axios.get('http://localhost:4000/users/employees')
+      .then(response => {
+        console.log(response.data);
+        const myRepo = response.data;
+        setRepo(myRepo);
+      });
+  };
+  
+
+  useEffect(() => getRepo(),[]);
+
+  const Eids = [
+    { title: 'Scott R Todd', Eid: 3006 },
+    { title: 'Terry J Moore', Eid: 3004  },
+    { title: 'Brittany D Bard', Eid: 3003  },
+    { title: 'James D Reed', Eid: 3002  },
+    { title: 'Marc L Benton', Eid: 3008  },
+  ];
 
     return (
       <div>
+        { repo.map((repos) => (
         <div className="prof">
           <h2>New Tasks</h2>
           <TextField id="standard-uncontrolled" label="Title" defaultValue="" /><br></br><br></br>
@@ -24,7 +43,7 @@ function NewTasks() {
             size="small"
             options={Eids}
             getOptionLabel={(option) => option.title}
-            defaultValue={[Eids[3]]}
+            defaultValue={[Eids[0]]}
             renderInput={(params) => (
               <TextField 
                 {...params}
@@ -38,19 +57,15 @@ function NewTasks() {
           <TextField id="outlined-multiline-flexible" label="Job Description" multiline Rows={4} variant="outlined" style = {{width: 350}} defaultValue=""/><br></br><br></br>
           <TextField id="standard-uncontrolled" label="Price" defaultValue="" /><br></br><br></br>
           <Button variant="contained" color="secondary">
-          Add Task
+          Add Task {repos.name}
           </Button><br></br><br></br>
         </div>
+        ))}
       </div>
+
     )
   }
-  
-  const Eids = [
-    { title: 'Scott R Todd', Eid: 3006 },
-    { title: 'Terry J Moore', Eid: 3004  },
-    { title: 'Brittany D Bard', Eid: 3003  },
-    { title: 'James D Reed', Eid: 3002  },
-    { title: 'Marc L Benton', Eid: 3008  },
-  ];
-  
+
+
   export default NewTasks
+
