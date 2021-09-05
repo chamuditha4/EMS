63975ab0f6b9c53e0e69b0203881591380e0fb31
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-05 22:09:36 +0530

    Fixed Everythin on New Task :)

--- src/Functions/Submission/Tasks/NewTasks.js
+++ src/Functions/Submission/Tasks/NewTasks.js
@@ -5,35 +5,31 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import axios from 'axios';
 
-
-
 function NewTasks() {
   const [repo,setRepo] = useState([]);
+  var Eids = [];
+
 
   const getRepo = () => {
     axios.get('http://localhost:4000/users/employees')
       .then(response => {
-        console.log(response.data);
+       // console.log(JSON.stringify(response.data));
         const myRepo = response.data;
         setRepo(myRepo);
+        return Eids;
       });
   };
   
 
   useEffect(() => getRepo(),[]);
 
-  const Eids = [
-    { title: 'Scott R Todd', Eid: 3006 },
-    { title: 'Terry J Moore', Eid: 3004  },
-    { title: 'Brittany D Bard', Eid: 3003  },
-    { title: 'James D Reed', Eid: 3002  },
-    { title: 'Marc L Benton', Eid: 3008  },
-  ];
+  
 
     return (
       <div>
         { repo.map((repos) => (
         <div className="prof">
+          <script>{Eids.push(repos)}</script>
           <h2>New Tasks</h2>
           <TextField id="standard-uncontrolled" label="Title" defaultValue="" /><br></br><br></br>
           <Autocomplete
@@ -42,7 +38,7 @@ function NewTasks() {
             limitTags={1}
             size="small"
             options={Eids}
-            getOptionLabel={(option) => option.title}
+            getOptionLabel={(option) => option.name}
             defaultValue={[Eids[0]]}
             renderInput={(params) => (
               <TextField 
@@ -57,7 +53,7 @@ function NewTasks() {
           <TextField id="outlined-multiline-flexible" label="Job Description" multiline Rows={4} variant="outlined" style = {{width: 350}} defaultValue=""/><br></br><br></br>
           <TextField id="standard-uncontrolled" label="Price" defaultValue="" /><br></br><br></br>
           <Button variant="contained" color="secondary">
-          Add Task {repos.name}
+          Add Task {console.log(repos)}
           </Button><br></br><br></br>
         </div>
         ))}
