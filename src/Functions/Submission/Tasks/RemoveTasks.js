570f4d11655aa4671399758f33fbdcc5507c3f5e
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-07 01:27:10 +0530

    remove func

--- src/Functions/Submission/Tasks/RemoveTasks.js
+++ src/Functions/Submission/Tasks/RemoveTasks.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState  } from 'react';
 import Button from '@material-ui/core/Button';
 import  './../../../styles/App.css';
+import axios from 'axios';
+import { getUser } from './../../../Utils/Common';
 
 function RemoveTasks() {
+  const user = getUser();
+  const [repo,setRepo] = useState([]);
+  const [Id, setId] = useState('');
+
+  const getRepo = () => {
+    axios.get('http://localhost:4000/tasks/' + user._id)
+      .then(response => {
+       // console.log(JSON.stringify(response.data));
+        const myRepo = response.data;
+        setRepo(myRepo);
+      });
+  };
+
+  function handleChange(e) {
+    console.log(e.target.selectedOptions[0].getAttribute('data-id'));
+    document.getElementById("removing").innerHTML = "You are removing: " + e.target.selectedOptions[0].getAttribute('data-id');
+    
+  }
+
+  useEffect(() => getRepo(),[]);
     return (
       <div>
         <div className="prof">
           <h2>Remove Tasks</h2>
-          <select>
-            <option value="908">Web Design [0098]</option>
-            <option value="408">Web Design [0097]</option>
-            <option value="508">Web Design [0096]</option>
+          <select  onChange={handleChange}>
+          <option value="Def" disabled selected="true">Select Task</option>
+          { repo.map((repos) => (
+            <option value={repos._id} data-id={repos.name} >{repos.name}</option>
+          ))}
           </select>
           <br></br><br></br>
-          <p>You removing: Web Design [0096] </p>
+          <p id="removing"></p>
           <br></br><br></br>
           <Button variant="contained" color="primary">
           Remove Task
