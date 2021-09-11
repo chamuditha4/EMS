ad65c3d08b93711a728e2af5bfb60e40eee1607a
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-11 13:01:38 +0530

    set sal

--- src/Functions/Salary/SalaryReport.js
+++ src/Functions/Salary/SalaryReport.js
@@ -1,47 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState  } from 'react';
 import  './../../styles/App.css';
+import axios from 'axios';
 
-function SalaryReport() {
+ function SalaryReport() {
+  const [repo,setRepo] = useState([]);
+  const [name1,setname] = useState([]);
+
+  const getRepo = () => {
+    axios.get('http://localhost:4000/salary')
+      .then(response => {
+       // console.log(JSON.stringify(response.data));
+        const myRepo = response.data;
+        setRepo(myRepo);
+      });
+  };
+
+  async function getuser(usern){
+    await axios.get('http://localhost:4000/users/get-user/'+ usern)
+      .then(response => {
+       // console.log(JSON.stringify(response.data));
+        const myRepo1 = response.data;
+        setname(myRepo1[0]);
+      });
+  }
+
+  useEffect(() => getRepo(),[]);
     return (
       <div>
         <div className="prof">
           <h2>Salary Report</h2>
           <table>
           <tr>
-            <th>Name</th>
             <th>ID</th>
             <th>Salary</th>
           </tr>
+          { repo.map((repos) => (
           <tr>
-            <td>A B C Jayasena</td>
-            <td>IT234</td>
-            <td>Rs. 45,000</td>
+            <td>{repos.eid}</td>
+            <td>{(parseInt(repos.salary)+parseInt(repos.bonus))} LKR</td>
           </tr>
-          <tr>
-            <td>.</td>
-            <td>.</td>
-            <td>.</td>
-          </tr>
-          <tr>
-            <td>.</td>
-            <td>.</td>
-            <td>.</td>
-          </tr>
-          <tr>
-            <td>.</td>
-            <td>.</td>
-            <td>.</td>
-          </tr>
-          <tr>
-            <td>.</td>
-            <td>.</td>
-            <td>.</td>
-          </tr>
-          <tr>
-            <td>.</td>
-            <td>.</td>
-            <td>.</td>
-          </tr>
+          ))}
         </table>
         <br></br>
         </div>
