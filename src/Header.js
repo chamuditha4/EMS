762941507716ecad92112d6d5cb9797e4b8c89f4
Capsy <71144025+Capsy001@users.noreply.github.com>
2021-09-21 01:51:53 +0530

    footer fix

--- src/Header.js
+++ src/Header.js
@@ -9,22 +9,7 @@ function Header() {
       <nav>
             <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="yy.html">Dashboard</a>
-                <ul>
-                <li><a href="Login.html">Mark Attendance</a></li>
-                <li><a href="Login.html">Announcements</a></li>
-
-                </ul>
-            </li>
-            <li><a href="Login.html">Find Us</a></li>
-            <li><a href="Login.html" class="active">About Us</a>
-                <ul>
-                <li><a href="Login.html">Our Story</a></li>
-                <li><a href="Login.html">Our Mission</a></li>
-                <li><a href="Login.html">Where We Work</a></li>
-                </ul>
-            </li>
-            <li><a href="contactUs.html">Contact Us</a></li>
+           
             </ul>
         </nav>
         <img src={logo} id="logo" alt="Logo"></img>
