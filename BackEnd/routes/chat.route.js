7938ea7ab265db0b7db3e84606cc993653472913
chamuditha4 <36603594+chamuditha4@users.noreply.github.com>
2021-09-21 23:07:47 +0530

    chat

--- BackEnd/routes/chat.route.js
+++ BackEnd/routes/chat.route.js
@@ -32,65 +32,6 @@ router.route('/create-chat').post((req, res, next) => {
 })
 });
 
-// READ Announcement
-router.route('/:user').get((req, res) => {
-  ChatSchema .find({eid:req.params.user},(error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
 
-// READ Announcement
-router.route('/read/:dept').get((req, res) => {
-  ChatSchema .find({department:req.params.dept},(error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
-
-router.route('/get-announcement/:id').get((req, res) => {
-  ChatSchema.findById(req.params.id, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
-
-// Update Announcement
-router.route('/update-announcement/:id').put((req, res, next) => {
-  ChatSchema.findByIdAndUpdate(req.params.id, {
-    $set: req.body
-  }, (error, data) => {
-    if (error) {
-      console.log(error)
-      return next(error);
-      
-    } else {
-      res.json(data)
-      console.log('Task updated successfully !')
-    }
-  })
-})
-
-// Delete announcement
-router.route('/delete-announcement/:id').delete((req, res, next) => {
-  ChatSchema.findByIdAndRemove(req.params.id, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.status(200).json({
-        msg: data
-      })
-    }
-  })
-})
 
 module.exports = router;
